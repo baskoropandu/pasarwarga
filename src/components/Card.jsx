f9182fffbf3c1560db6bf6662960cd05bc1e4f38
baskoropandu <baskoro.p.g@gmail.com>
2021-10-24 16:57:48 +0700

    integrating 2

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,20 +1,16 @@
 import React, { useEffect, useState } from "react";
 import { useDispatch } from "react-redux";
-import { useParams } from "react-router";
+import { useHistory, useParams } from "react-router";
 import numberToDay from "../../helpers/numberToDay";
 
 
 export default function Card() {
-  const {id} = useParams()
-  const dispatch = useDispatch()
-  const [details, setDetails] = useState({})
-  useEffect(()=>{
-    setDetails(dispatch(getDetails(id)))
-  },[id])
+  const 
+  
   return (
     <div class="p-4">
           <div class="h-full border-2 border-gray-400 border-opacity-60 bg-gray-500 bg-opacity-20 rounded-lg overflow-hidden">
-            <div class="lg:h-48 md:h-36 w-full object-cover object-center" src="https://dummyimage.com/720x400" alt="blog"/>
+            <div class="lg:h-48 md:h-36 w-full object-cover object-center" style={{backgroundImage: `url("${details.image}")`}}/>
             <div class="p-6">
               <h1 class="title-font text-lg font-medium text-gray-900 mb-3">{details.title}</h1>
               <div class="flex">
@@ -40,13 +36,13 @@ export default function Card() {
                 </p>
               </div>
               <div class="flex items-center flex-wrap ">
-                <a class="text-blue-500 inline-flex items-center md:mb-2 lg:mb-0">
+                <button onClick={()=> useHistory().push(`/articles/${details.id}`)} class="text-blue-500 inline-flex items-center md:mb-2 lg:mb-0">
                   Learn More
                   <svg class="w-4 h-4 ml-2" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round">
                     <path d="M5 12h14"></path>
                     <path d="M12 5l7 7-7 7"></path>
                   </svg>
-                </a>
+                </button>
               </div>
             </div>
           </div>
